e012fa0e2706b971ff72ced0a1af8191001bdf6b
Dev15Durgam <blackros_91@hotmail.com>
2016-11-23 15:13:18 +0100

    Search & Create new (Problem with Calander)

---
+++ WebApplication1/agr_Create_New.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Portal
+{
+    public partial class agr_Create_New : System.Web.UI.Page
+    {
+        Controllers.DefaultController dp = new Controllers.DefaultController();
+        DataTable dt = new DataTable(); // declearing dt as a global variable.
+
+        internal void Page_Load(object sender, EventArgs e)
+        {
+            //Use this to stop the page from backposting? ---> if (!Page.IsPostBack) { GetData(); }
+
+            LoginAuthentication(); // checks if the user is logedin via logon site before showing this site.
+
+        }
+        private void LoginAuthentication()
+        {
+            if (Controllers.login.data != null)
+                Controllers.login.ConstructNavigator(Navigator);
+            else { Response.Redirect("Logon.aspx"); }
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+
+            ImageButton ib = sender as ImageButton;
+
+            if (FindControl("Vaild_From_Calendar").Visible==false)
+            {
+                FindControl("Vaild_From_Calendar").Visible = true;
+            }
+            else if (FindControl("Vaild_From_Calendar").Visible == true)
+            {
+                FindControl("Vaild_From_Calendar").Visible = false;
+            }
+            ScriptManager.GetCurrent(this).RegisterAsyncPostBackControl(ib);
+
+
+        }
+
+        protected void Vaild_From_Calendar_VisibleMonthChanged(object sender, MonthChangedEventArgs e)
+        {
+            TextBox textbox = (TextBox)txtBox_New_Agr_Valid_From.FindControl("Vaild_From_Calendar");
+            ScriptManager.GetCurrent(this).RegisterAsyncPostBackControl(textbox);
+        }
+
+        protected void Vaild_From_Calendar_SelectionChanged(object sender, EventArgs e)
+        {
+            string txtVaildFrom = txtBox_New_Agr_Valid_From.ToString();
+            txtBox_New_Agr_Valid_From.Text = Vaild_From_Calendar.SelectedDate.ToString();
+            ScriptManager.GetCurrent(this).RegisterAsyncPostBackControl(txtBox_New_Agr_Valid_From);
+        }
+    }
+}
