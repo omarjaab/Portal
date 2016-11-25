3192b8d597e5dc78511ed5249c6f6f65f4ba144d
Ahmed <dj_sweden_20@hotmail.com>
2016-11-25 11:00:04 +0100

    ahmed3

--- WebApplication1/agr_Create_New.aspx.cs
+++ WebApplication1/agr_Create_New.aspx.cs
@@ -19,7 +19,7 @@ namespace Portal
             //Use this to stop the page from backposting? ---> if (!Page.IsPostBack) { GetData(); }
 
             LoginAuthentication(); // checks if the user is logedin via logon site before showing this site.
-            //ahmed 2
+            //ahmed 3
         }
         private void LoginAuthentication()
         {
